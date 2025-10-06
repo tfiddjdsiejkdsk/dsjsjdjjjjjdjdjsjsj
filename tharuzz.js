395a76c6f1385e476c1e25992fbfd2959d0d709e
tfiddjdsiejkdsk <renderfreeplan@gmail.com>
2025-10-06 12:52:51 +0530

    Update tharuzz.js

--- tharuzz.js
+++ tharuzz.js
@@ -1,1 +1,22 @@
+const axios = require('axios');
 
+// Fetch JSON from a URL
+const fetchJson = async (url, options) => {
+    try {
+        options = options || {};
+        const res = await axios({
+            method: 'GET',
+            url: url,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+            },
+            ...options
+        });
+        return res.data;
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+};
+
+module.exports = { fetchJson };
